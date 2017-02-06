e5b67607783a825e657ca2d4d847517f4b7cc31a
charles <b088081@gmail.com>
2017-02-06 14:52:14 +0900

    주소록 수정 기능 추가, 리액티비티 활용,

--- client/addressList.js
+++ client/addressList.js
@@ -21,14 +21,23 @@ Template.addressList.helpers({
 });
 
 Template.addressList.events({
-  'click button[name=remove]' (evt,tmpl){
-    AddressBook.remove({_id : this._id});
-}
 });
 
-Template.addressInput.events({
-  'click button[name=saveAddress]' (evt,tmpl){
-      console.log(tmpl);
+Template.addressListItem.helpers({
+  editing(){
+    return this._id == Session.get("editItem");
+  }
+});
+
+Template.addressListItem.events({
+  'click button[name=remove]' (evt,tmpl){
+    AddressBook.remove({_id : this._id});
+  },
+  'click button[name=modify]' (evt,tmpl){
+    Session.set("editItem",this._id);
+  },
+  'click button[name=save]' (evt,tmpl){
+      /* input 박스 값으로 입력 데이터 만들기*/
       var address = {
         name : tmpl.find("input[name=name]").value,
         phone : tmpl.find("input[name=phone]").value,
@@ -37,9 +46,33 @@ Template.addressInput.events({
         birthday : tmpl.find("input[name=birthday]").value
       };
       console.log(address);
+      /* insert 하기 */
+      AddressBook.update({_id:this._id},{$set:address});
+      Session.set("editItem",null);
+  },
+  'click button[name=cancel]' (evt,tmpl){
+    Session.set("editItem", null);
+  },
+  'click.edit-thing' (evt,tmpl){
+    Session.set("editItem", this._id);
+  }
+});
 
+Template.addressInput.events({
+  'click button[name=saveAddress]' (evt,tmpl){
+      /* input 박스 값으로 입력 데이터 만들기*/
+      var address = {
+        name : tmpl.find("input[name=name]").value,
+        phone : tmpl.find("input[name=phone]").value,
+        email : tmpl.find("input[name=email]").value,
+        company : tmpl.find("input[name=company]").value,
+        birthday : tmpl.find("input[name=birthday]").value
+      };
+      console.log(address);
+      /* insert 하기 */
       AddressBook.insert(address);
 
+       /* input 값 초기화 하기 */
        tmpl.find("input[name=name]").value = "";
        tmpl.find("input[name=phone]").value = "";
        tmpl.find("input[name=email]").value = "";
