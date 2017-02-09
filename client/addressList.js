abd4cd0f4c02dcd1b56c97f5ee4dabde2a26b977
charles <b088081@gmail.com>
2017-02-09 17:11:40 +0900

    주소록 입력,수정 유효성 검사, 보안 적용, 로그인,회원가입 구현

--- client/addressList.js
+++ client/addressList.js
@@ -1,3 +1,7 @@
+Template.addressList.onCreated(function(){
+  var self = this;
+  self.subscribe("AddressBookData",10);
+});
 Template.addressList.helpers({
   // list:[
   //   {
@@ -46,6 +50,16 @@ Template.addressListItem.events({
         birthday : tmpl.find("input[name=birthday]").value
       };
       console.log(address);
+      try{
+        check(address.name , NotEmpthString);
+        check(address.company , NotEmpthString);
+        check(address.email , EmailString);
+        check(address.phone , PhoneString);
+        check(address.birthday , BirthDayString);
+      }catch(err){
+        alert("입력값을 확인하세요 : [" + err.message + "]");
+        return ;
+      }
       /* insert 하기 */
       AddressBook.update({_id:this._id},{$set:address});
       Session.set("editItem",null);
@@ -61,14 +75,50 @@ Template.addressListItem.events({
 Template.addressInput.events({
   'click button[name=saveAddress]' (evt,tmpl){
       /* input 박스 값으로 입력 데이터 만들기*/
+      console.log(tmpl);
       var address = {
         name : tmpl.find("input[name=name]").value,
         phone : tmpl.find("input[name=phone]").value,
         email : tmpl.find("input[name=email]").value,
         company : tmpl.find("input[name=company]").value,
-        birthday : tmpl.find("input[name=birthday]").value
+        birthday : tmpl.find("input[name=birthday]").value,
+        owner : Meteor.userId()
       };
       console.log(address);
+      /* 데이터 검증하기 */
+      NotEmpthString = Match.Where((x)=>{
+        check(x , String);
+        return x.length > 0;
+      });
+
+        /* 이메일 */
+      EmailString = Match.Where((x)=>{
+            check(x,String);
+            return /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/.test(x);
+        });
+
+      /* 전화번호 : "000-0000-0000" */
+      PhoneString = Match.Where((x)=>{
+          check(x,String);
+          return /^\d{3}-\d{3,4}-\d{4}$/.test(x);
+      });
+
+      /* 특정 날짜 형식 : "yyyy/mm/dd" */
+      BirthDayString = Match.Where((x)=>{
+          check(x,String);
+          return  /^(19[0-9][0-9]|20\d{2})\/(0[0-9]|1[0-2])\/(0[1-9]|[1-2][0-9]|3[0-1])$/.test(x);
+      });
+      try{
+        check(address.name , NotEmpthString);
+        check(address.company , NotEmpthString);
+        check(address.email , EmailString);
+        check(address.phone , PhoneString);
+        check(address.birthday , BirthDayString);
+      }catch(err){
+        alert("입력값을 확인하세요 : [" + err.message + "]");
+        return ;
+      }
+
       /* insert 하기 */
       AddressBook.insert(address);
 
